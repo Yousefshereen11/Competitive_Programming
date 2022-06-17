45911edb177bbe60ef39a4425f3a8697e631800e
Yousefshereen11 <77634908+Yousefshereen11@users.noreply.github.com>
2022-06-17 20:17:10 +0200

    Update BIT.cpp

--- Template/BIT.cpp
+++ Template/BIT.cpp
@@ -1,18 +1,29 @@
-const int N = 1e6;
-int BIT[N];
-void add(int idx, int val){
-    while (idx < N)
-        BIT[idx] += val, idx += (idx & -idx);
-}
-void add(int l, int r, int val){
-    add(l, val), add(r + 1, -val);
-}
-int get(int idx){
-    int sum = 0;
-    while(idx > 0)
-        sum += BIT[idx], idx -= (idx & -idx);
-    return sum;
-}
-int get(int l, int r){
-    return get(r) - get(l - 1);
-}
+template< typename T, T(*combine)(T, T) >
+struct fenwickTree {
+    int n;
+    vector< T > BIT;
+    T initialValue;
+    fenwickTree(int sz, T initialValue = 0) : n(sz + 5), initialValue(initialValue) {
+        BIT.assign(n, initialValue);
+    }
+
+    T query(int right) {
+        T ans = initialValue;
+        for (; right >= 0; right = (right & (right + 1)) - 1)
+            ans = combine(ans, BIT[right]);
+        return ans;
+    }
+
+    T query(int left, int right) { // for +, - only
+        return query(right) - query(left - 1);
+    }
+
+    void add(int idx, T delta) {
+        for (; idx < n; idx = idx | (idx + 1))
+            BIT[idx] = combine(BIT[idx], delta);
+    }
+    void add(int left, int right, T delta){
+        add(left, delta);
+        add(right + 1, -delta);
+    }
+};
